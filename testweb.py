a459e9e92f67a241975ed3dbaf22439574d84e1f
kmartinez <km@ecs.soton.ac.uk>
2020-11-03 16:06:29 +0000

    proto web server demo

--- testweb.py
+++ testweb.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
-
+import time
+import explorerhat as hat
 from bottle import route, run
+
 @route('/')
 def hello():
     return("hello")
+
+@route('/flash')
+def flash():
+    hat.light.yellow.on()
+    time.sleep(0.2)
+    hat.light.yellow.off()
+    return('flashed')
 
 run(host='0.0.0.0', port=8080, debug=True)
