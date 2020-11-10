5216a1ebbbb0aecdab4e766b7347801c71a69eea
kmartinez <km@ecs.soton.ac.uk>
2020-11-10 17:56:57 +0000

    added py3 way to get cpu temp

--- testweb.py
+++ testweb.py
@@ -2,6 +2,7 @@
 import time
 import explorerhat as hat
 from bottle import route, run
+import subprocess as sub
 
 @route('/')
 def hello():
@@ -13,5 +14,10 @@ def flash():
     time.sleep(0.2)
     hat.light.yellow.off()
     return('flashed')
+
+@route('/cputemp')
+def index(name='cputemp'):
+    result = sub.run(["vcgencmd","measure_temp"], capture_output=True)
+    return(result.stdout)
 
 run(host='0.0.0.0', port=8080, debug=True)
