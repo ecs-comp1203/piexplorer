caa6ecf253a3a2c5b6f6151b54247263ba5eaf0f
kmartinez <km@ecs.soton.ac.uk>
2020-11-12 09:07:25 +0000

    fixed to Blue led GPIO thanks Elisha

---
+++ pi-gpio-led.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# how to control leds from normal Pi module
+
+import time
+import RPi.GPIO as GPIO
+import time
+pin = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pin,GPIO.OUT)
+GPIO.output(pin,GPIO.HIGH)
+time.sleep(1)
+GPIO.output(pin,GPIO.LOW)
+
