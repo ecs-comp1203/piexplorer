87b53c6509e19a17d52e39049700c2d9ae937bf7
kmartinez <km@ecs.soton.ac.uk>
2020-11-30 17:11:37 +0000

    asm1: simple operation returning to shell

---
+++ arm/asm1.s
@@ -0,0 +1,10 @@
+# simple operation returning result in r0 which is the return value to shell
+# so echo $? will print it afterwards (only one byte)
+.arm
+.text
+.global main
+main:
+	ldr r0, =200
+	mov r1, #4
+	add r0, r0, r1
+	bx lr
